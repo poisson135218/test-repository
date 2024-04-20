cef70150fdaf04de4929ab73ac51fde82998b9e6
poisson135218 <poisson0315@gmail.com>
2024-04-20 15:08:04 +0900

    編集機能の追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,7 +16,11 @@
                 <p class='body'>{{ $post->body }}</p>
             </div>
         </div>
+        <div class='edit'>
+             <a href="/posts/{{ $post->id }}/edit">edit</a>
+        </div>
         <div class='footer'>
-             <a href="/">戻る</a></div>
+             <a href="/">戻る</a>
+        </div>
     </body>
 </html>
